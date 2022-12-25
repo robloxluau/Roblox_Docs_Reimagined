71cbb7f5e3ccd4a4f375ee99839795690b6f0896
Expired <developingbread@gmail.com>
2022-12-24 17:10:30 -0800

    Embed

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,6 +4,16 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Roblox Docs Reimagined</span>,
 
+  useNextSeoProps() {
+    return { titleTemplate: '%s - Roblox Docs Reimagined' }
+  },
+
+  head: ( <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta property="og:title" content="Roblox Docs Reimagined" />
+    <meta property="og:description" content="Roblox Documents in depth" />
+  </>),
+
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
 
   toc: { float: true },
