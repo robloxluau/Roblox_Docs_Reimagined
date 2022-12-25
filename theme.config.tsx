3ded5dbe5d744ddfa02efac7edc96498fbc6c60b
Expired <developingbread@gmail.com>
2022-12-24 17:02:12 -0800

    OOP Documentation

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,15 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
+  logo: <span>Roblox Docs Reimagined</span>,
+
   docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+
+  toc: { float: true },
+  search: { loading: 'Tring to look...', placeholder: 'Search: Luau is the best 😎' },
+
+  project: { link: 'https://github.com/shuding/nextra-docs-template', },
+  footer: { text: 'Roblox Luau Docs Reimagined | under the MIT license', },
 }
 
 export default config
